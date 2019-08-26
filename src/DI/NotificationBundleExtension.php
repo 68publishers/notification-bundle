d5741ec2236e66e8b224cf1c8e846b54c266e927
Tomáš Glawaty <tomasglawaty@icloud.com>
2019-08-26 20:53:25 +0200

    DI - component's templates
    - added configuration options `templates.flash_message` and `templates.toastr`

--- src/DI/NotificationBundleExtension.php
+++ src/DI/NotificationBundleExtension.php
@@ -12,6 +12,10 @@ final class NotificationBundleExtension extends Nette\DI\CompilerExtension
 	/** @var array  */
 	private $defaults = [
 		'storage' => NULL,
+		'templates' => [
+			'flash_message' => NULL,
+			'toastr' => NULL,
+		],
 	];
 
 	/**
@@ -29,6 +33,7 @@ final class NotificationBundleExtension extends Nette\DI\CompilerExtension
 			->setAutowired(FALSE);
 
 		Nette\Utils\Validators::assertField($config, 'storage', sprintf('null|string|%s', Nette\DI\Statement::class));
+
 		if (NULL !== ($storage = $config['storage'])) {
 			$provider->setArguments([
 				'storage' => $storage instanceof Nette\DI\Statement ? $storage : new Nette\DI\Statement($storage),
@@ -53,11 +58,23 @@ final class NotificationBundleExtension extends Nette\DI\CompilerExtension
 				'provider' => $provider,
 			]);
 
-		$builder->addDefinition($this->prefix('flash_message_control'))
+		$flashMessageControl = $builder->addDefinition($this->prefix('flash_message_control'))
 			->setImplement(SixtyEightPublishers\NotificationBundle\Control\FlashMessage\IFlashMessageControlFactory::class);
 
-		$builder->addDefinition($this->prefix('toastr_control'))
+		$toastrControl = $builder->addDefinition($this->prefix('toastr_control'))
 			->setImplement(SixtyEightPublishers\NotificationBundle\Control\Toastr\IToastrControlFactory::class);
+
+		if (NULL !== $config['templates']['flash_message']) {
+			$flashMessageControl->addSetup('setFile', [
+				$config['templates']['flash_message'],
+			]);
+		}
+
+		if (NULL !== $config['templates']['toastr']) {
+			$toastrControl->addSetup('setFile', [
+				$config['templates']['toastr'],
+			]);
+		}
 	}
 
 	/**
